c5a4781e87c2751625bba0972280ff4e7a71fde2
Marco <marcocunha838@gmail.com>
2021-05-23 08:46:42 -0300

    Revert "Criada a Classe empréstimo"
    This reverts commit 8c7e8ceb3f0fbf3a59a59bdc30badc19021bf9f1.

--- src/biblioteca/BibliotecaApp.java
+++ src/biblioteca/BibliotecaApp.java
@@ -5,9 +5,6 @@ public class BibliotecaApp {
 	public static void main(String[] args) {
 		
 	Livro Livro1 = new Livro();
-	Usuario u = new Usuario();
-	
-	Emprestimo emprestimo = new Emprestimo();
 				
 	}
 
